c8f785fb42d570c47c68331830b93df339372cbe
keerthanamathavan <keerthiya27694@gmail.com>
2017-03-02 11:04:34 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class large
+{
+  public static void main(String args[])
+  {
+     int num[]={20,70,10,5,60};
+     int largest=num[0];
+     for(int i=0;i<num.lenght,i++)
+     {
+        if(num[i]>largest)
+        largest=num[i];
+     }
+     System.out.println("Largest Number is : " + largest);
+   }
+  }
+     
